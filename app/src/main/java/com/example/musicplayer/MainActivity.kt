ae754010eafc033453b5f2224bbd38a5e4c5aa53
denden-kata <denden3104@gmail.com>
2019-10-28 22:58:36 +0900

    ボタン追加

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val playButton: Button = findViewById(R.id.playBtn)
+        val elapsedTimeLabel: TextView = findViewById(R.id.elapsedTimeLabel)
+        val remainingTimeLabel: TextView = findViewById(R.id.remainingTimeLabel)
+        val volumeBar: SeekBar
+        val positionBar: SeekBar
+        val mp: MediaPlayer
+
+
     }
 }
