9268cbe9e467fe8584f660e97f9dc1eb15670266
denden-kata <denden3104@gmail.com>
2019-10-31 23:42:33 +0900

    再生機能を追加

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -3,27 +3,48 @@ package com.example.musicplayer
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
+    private lateinit var playButton: Button
+    private lateinit var elapsedTimeLabel: TextView
+    private lateinit var remainingTimeLabel: TextView
+
+    private lateinit var volumeBar: SeekBar
+    private lateinit var positionBar: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val playButton: Button = findViewById(R.id.playBtn)
-        val elapsedTimeLabel: TextView = findViewById(R.id.elapsedTimeLabel)
-        val remainingTimeLabel: TextView = findViewById(R.id.remainingTimeLabel)
-        val volumeBar: SeekBar
-        val positionBar: SeekBar
-        val mp: MediaPlayer = MediaPlayer.create(this, R.raw.test01)
 
+        mp = MediaPlayer.create(this, R.raw.test01)
+        playButton = findViewById(R.id.playBtn)
+        elapsedTimeLabel = findViewById(R.id.elapsedTimeLabel)
+        remainingTimeLabel = findViewById(R.id.remainingTimeLabel)
 
         // Media Player の初期化
         mp.isLooping
         mp.seekTo(0)
         mp.setVolume(0.5f, 0.5f)
+    }
+
+    public fun playBtnClick(view: View){
+        if(!mp.isPlaying) {
+            // 停止中なら再生する
+            mp.start()
+            playBtn.setBackgroundResource(R.drawable.stop)
+
+        } else {
+            // 再生中なら停止する
+            mp.pause()
+            playBtn.setBackgroundResource(R.drawable.play)
+        }
     }
 }
